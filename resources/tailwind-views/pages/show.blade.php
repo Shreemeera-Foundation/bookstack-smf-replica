d530e45708d88034c8996d684ef198706af7ff06
Nupur Lagu <nupur.lagu@shreemeera.org>
2023-01-31 23:23:52 +0530

    Hide comments for all the users

--- resources/tailwind-views/pages/show.blade.php
+++ resources/tailwind-views/pages/show.blade.php
@@ -34,7 +34,7 @@
             </div>
         @endif
 
-        <div class="px-xl comments-container mb-l print-hidden">
+        <div class="px-xl comments-container mb-l print-hidden smf-hidden">
             @include('comments.comments', ['page' => $page])
             <div class="clearfix"></div>
         </div>
